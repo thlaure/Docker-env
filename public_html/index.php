c7c4d12ede11928b23636107a112c942ef1283de
Thomas LAURE <thomas.laure@ik.me>
2022-07-01 23:10:27 +0200

    C'est cool

--- public_html/index.php
+++ public_html/index.php
@@ -1,3 +1,23 @@
 <?php
 
+// echo phpinfo();
+
 echo 'Hello, World!';
+
+$host = 'mariadb:3306';
+$dbname = 'mydatabase';
+$user = 'root';
+$password = 'password';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+    $stmt = $pdo->query('SELECT * FROM visitor', PDO::FETCH_ASSOC);
+    $results = $stmt->fetchAll();
+    
+    echo '<pre>';
+    var_dump($results);
+    echo '</pre>';
+}
+catch (PDOException $e) {
+    echo $e->getMessage();
+}
