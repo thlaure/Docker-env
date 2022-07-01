dd4e660fc74b5215f26a6ee753547beacc39cf41
Laure <t.laure@la-boite-immo.com>
2022-07-01 17:00:38 +0200

    Installation PMA

--- public_html/index.php
+++ public_html/index.php
@@ -1,7 +1,3 @@
 <?php
 
-//Environmental variables defined in .env file
-$instanceName = $_SERVER['INSTANCE_NAME'];
-$projectColor = $_SERVER['PROJECT_COLOR'];
-
-echo '<h1>Hello from <font color="'.$projectColor.'">'.$instanceName.'</font> instance</h1>';
+echo 'Hello, World!';
